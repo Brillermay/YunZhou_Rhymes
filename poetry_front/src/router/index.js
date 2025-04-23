5e4595de7da5f2d2e5074e67eb97eee983cd2f7a
zky <145536006+Yaoyaolaogong@users.noreply.github.com>
2025-04-23 16:25:47 +0800

    为了适配poemdetail.vue

--- poetry_front/src/router/index.js
+++ poetry_front/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import PoetryRecommend from '@/components/PoetryRecommend.vue'
 import PoetrySearch from '@/components/PoetrySearch.vue'
+import PoemDetail from '@/components/PoemDetail.vue';
 import PoetryGame from '@/components/PoetryTest.vue'
 import Forum from '@/components/Forum.vue'
 import feihua from '@/components/FeiHuaLing.vue'
@@ -12,6 +13,7 @@ const router = createRouter({
     {path: '/', redirect: '/recommend'},
   { path: '/recommend', component: PoetryRecommend },
   { path: '/search', component: PoetrySearch },
+  { path: '/poem/:id', component: PoemDetail },
   { path: '/game', component: PoetryGame },
   { path: '/feihua', component: feihua },
   { path: '/forum', component: Forum }
